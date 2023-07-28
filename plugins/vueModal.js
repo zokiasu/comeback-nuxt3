7d35b45d1850daad11d73f6a696f0540ee3b0e2c
Pierrick Taïly <pierrick.tly@gmail.com>
2023-07-28 13:50:27 +0200

    fix

--- plugins/vueModal.js
+++
@@ -1,7 +0,0 @@
-import { Modal } from '@kouts/vue-modal'
-import '@kouts/vue-modal/dist/vue-modal.css'
-
-export default defineNuxtPlugin(nuxtApp => {
-  const Mdl = Modal
-  nuxtApp.vueApp.use(Mdl);
-})
