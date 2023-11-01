6aea60440a653735eb665a1fa5437a12c041a9a9
Pierrick Taïly <pierrick.tly@gmail.com>
2023-11-01 08:03:56 +0100

    Create HomeSlider.vue

---
+++ components/HomeSlider.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <section v-if="newsToday.length && newsFetched">
+      <div class="relative">
+        <div class="absolute z-10 pt-10">
+          <p
+            class="w-fit bg-red-700 py-1 pl-8 pr-5 text-xs font-semibold uppercase drop-shadow-lg lg:text-xl xl:text-2xl"
+          >
+            Comeback Today
+          </p>
+        </div>
+
+        <Swiper
+          :modules="[SwiperAutoplay, SwiperParallax]"
+          :slides-per-view="1"
+          :loop="true"
+          :parallax="true"
+          :autoplay="{
+            delay: 3500,
+            disableOnInteraction: false,
+          }"
+        >
+          <SwiperSlide
+            v-for="news in newsToday"
+            :key="news.id"
+            class="swiper-slide relative"
+          >
+            <NuxtImg
+              :src="news.artist.image"
+              class="min-h-[20rem] w-full object-cover lg:max-h-[40rem]"
+            />
+            <NuxtLink
+              :to="`/artist/${news.artist.id}`"
+              class="absolute inset-0 z-50 flex flex-col items-center justify-center bg-secondary/30 p-5"
+            >
+              <p
+                class="self-center text-3xl font-bold lg:text-5xl xl:text-7xl 2xl:text-9xl"
+              >
+                {{ news.artist.name }}
+              </p>
+            </NuxtLink>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+    <section
+      v-else
+      class="relative flex h-[calc(100vh-60px)] w-full flex-col justify-center bg-[url('https://www.blind-magazine.com/wp-content/uploads/2021/12/comment-photographier-un-concert-fr-1536x864.jpg.webp')] bg-cover bg-center bg-no-repeat text-center sm:min-h-[30rem] lg:max-h-[40rem]"
+    >
+      <div class="absolute inset-0 bg-black/60"></div>
+      <div class="z-10 space-y-1.5 xl:space-y-5">
+        <p class="text-[2rem] font-bold sm:text-[6vw] xl:text-7xl">
+          Don't miss any
+          <span class="text-primary">Comeback</span>
+        </p>
+        <p class="text-[3vw] xl:text-3xl">
+          Track every next release by your favorite artists
+        </p>
+      </div>
+      <p class="absolute bottom-20 left-0 right-0 md:hidden">
+        <icon-arrow-down class="mx-auto h-5 w-5 animate-bounce" />
+      </p>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  Timestamp,
+  collection,
+  onSnapshot,
+  query,
+  where,
+  orderBy,
+} from 'firebase/firestore'
+
+const { $firestore: db } = useNuxtApp()
+
+const news = ref([] as any[])
+
+const newsFetched = ref(false)
+
+const newsToday = computed(() => {
+  return news.value.filter((news: any) => {
+    const newsDate = new Date(news.date.seconds * 1000)
+    const today = new Date()
+    return (
+      newsDate.getDate() === today.getDate() &&
+      newsDate.getMonth() === today.getMonth() &&
+      newsDate.getFullYear() === today.getFullYear()
+    )
+  })
+})
+
+const newsFetching = () => {
+  const newsDate = new Date()
+  newsDate.setDate(newsDate.getDate() - 1)
+  const q = query(
+    collection(db as any, 'news'),
+    where('date', '>=', Timestamp.fromDate(newsDate)),
+    orderBy('date', 'asc'),
+  )
+  onSnapshot(q, (querySnapshot) => {
+    const newsTmp: any[] = []
+    querySnapshot.forEach((doc) => {
+      newsTmp.push(doc.data())
+    })
+    news.value = newsTmp
+    newsFetched.value = true
+  })
+}
+
+onMounted(async () => {
+  newsFetching()
+})
+</script>
