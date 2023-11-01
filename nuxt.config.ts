0f06686a78f1397e361473f7cddbd577112a2e4a
Pierrick Taïly <pierrick.tly@gmail.com>
2023-11-01 18:02:07 +0100

    remove vuefire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,6 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/image',
-    'nuxt-vuefire',
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
     'nuxt-swiper',
@@ -32,19 +31,6 @@ export default defineNuxtConfig({
   build: {
     // vue-toastification - old commonjs module
     transpile: ['vue-toastification', '@vuepic/vue-datepicker'],
-  },
-
-  vuefire: {
-    auth: true,
-    config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGIN_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    },
   },
 
   pinia: {
