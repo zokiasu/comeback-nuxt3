5aa034456651f7da4a588f0d1d350513ff695bc2
Zokiasu <pierrick.tly@gmail.com>
2024-03-09 20:36:16 +0100

    Refactor dashboard layout and components

--- plugins/VueDatePicker.js
+++ plugins/VueDatePicker.js
@@ -1,7 +1,13 @@
+// import * as vdp from '@vuepic/vue-datepicker'
+// import '@vuepic/vue-datepicker/dist/main.css'
+
+// export default defineNuxtPlugin((nuxtApp) => {
+//   const { VueDatePicker } = vdp
+//   nuxtApp.vueApp.use(VueDatePicker)
+// })
 import * as vdp from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const { VueDatePicker } = vdp
-  nuxtApp.vueApp.use(VueDatePicker)
+  nuxtApp.vueApp.component('VueDatePicker', vdp.VueDatePicker)
 })
