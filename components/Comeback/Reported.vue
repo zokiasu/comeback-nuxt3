7547f368e365dacc2fb8ab5bd7e012e78dbd8b18
Zokiasu <pierrick.tly@gmail.com>
2024-07-21 20:33:43 +0200

    refactor: Enable lazy loading for images in Comeback/Reported.vue component

--- components/Comeback/Reported.vue
+++ components/Comeback/Reported.vue
@@ -10,18 +10,22 @@
   const maxDisplay = ref(12)
 
   const comebackToDisplay = computed(() => {
-    return comebackList.slice(0, maxDisplay.value)
+    return displayAll.value ? comebackList : comebackList.slice(0, maxDisplay.value);
   })
+
+  const toggleDisplayAll = () => {
+    displayAll.value = !displayAll.value;
+  };
 </script>
 
 <template>
-  <CardDefault name="Comeback reported" :class="{ hidden: !comebackList }">
+  <CardDefault name="Comeback reported" :class="{ hidden: !comebackList.length }">
     <div
       v-if="comebackList.length"
       class="grid grid-cols-1 gap-2 mb-5 md:grid-cols-2 2xl:grid-cols-3"
     >
       <CardNews
-        v-for="comeback in comebackList"
+        v-for="comeback in comebackToDisplay"
         :key="comeback.id"
         :message="comeback.message"
         :date="comeback.date"
@@ -37,20 +41,12 @@
     </div>
     <div v-if="comebackList.length > maxDisplay" class="flex justify-center w-full">
       <button
-        v-if="!displayAll"
         type="button"
         class="flex items-center gap-1 p-1 font-semibold border rounded w-fit border-tertiary"
-        @click="displayAll = true"
+        @click="toggleDisplayAll"
       >
-        <IconPlus class="w-3 h-3 mx-auto" />
-      </button>
-      <button
-        v-else
-        type="button"
-        class="flex items-center gap-1 p-1 font-semibold border rounded w-fit border-tertiary"
-        @click="displayAll = false"
-      >
-        <IconMinus class="w-3 h-3 mx-auto" />
+        <IconPlus v-if="!displayAll" class="w-3 h-3 mx-auto" />
+        <IconMinus v-else class="w-3 h-3 mx-auto" />
       </button>
     </div>
   </CardDefault>
