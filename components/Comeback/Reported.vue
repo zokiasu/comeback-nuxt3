dc83321f92a9dd900a7471c73b72ef09b2064add
Zokiasu <pierrick.tly@gmail.com>
2024-07-21 20:56:37 +0200

    refactor: Enable lazy loading for images in Comeback/Reported.vue component

--- components/Comeback/Reported.vue
+++ components/Comeback/Reported.vue
@@ -1,27 +1,34 @@
 <script setup>
-  const { comebackList } = defineProps({
+  const props = defineProps({
     comebackList: {
       type: Array,
       required: true,
     },
-  })
+  });
 
-  const displayAll = ref(false)
-  const maxDisplay = ref(12)
+  const displayAll = ref(false);
+  const maxDisplay = ref(12);
 
   const comebackToDisplay = computed(() => {
-    return displayAll.value ? comebackList : comebackList.slice(0, maxDisplay.value);
-  })
+    return displayAll.value ? props.comebackList : props.comebackList.slice(0, maxDisplay.value);
+  });
 
   const toggleDisplayAll = () => {
     displayAll.value = !displayAll.value;
   };
+
+  // Pour déboguer
+  // watchEffect(() => {
+  //   console.log('comebackList', props.comebackList);
+  //   console.log('displayAll', displayAll.value);
+  //   console.log('comebackToDisplay', comebackToDisplay.value);
+  // });
 </script>
 
 <template>
-  <CardDefault name="Comeback reported" :class="{ hidden: !comebackList.length }">
+  <CardDefault name="Comeback reported" :class="{ hidden: !props.comebackList.length }">
     <div
-      v-if="comebackList.length"
+      v-if="props.comebackList.length"
       class="grid grid-cols-1 gap-2 mb-5 md:grid-cols-2 2xl:grid-cols-3"
     >
       <CardNews
@@ -34,12 +41,12 @@
     </div>
     <div v-else class="grid grid-cols-1 gap-2 py-5 md:grid-cols-2 2xl:grid-cols-3">
       <SkeletonDefault
-        v-for="i in maxDisplay"
+        v-for="i in maxDisplay.value"
         :key="`comeback_skeleton_` + i"
         class="h-12 rounded"
       />
     </div>
-    <div v-if="comebackList.length > maxDisplay" class="flex justify-center w-full">
+    <div v-if="props.comebackList.length > maxDisplay" class="flex justify-center w-full">
       <button
         type="button"
         class="flex items-center gap-1 p-1 font-semibold border rounded w-fit border-tertiary"
