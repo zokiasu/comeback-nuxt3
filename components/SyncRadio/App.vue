ca8dfb6553f4db40162816c298ab0bc19044e803
Zokiasu <pierrick.tly@gmail.com>
2024-07-27 00:41:03 +0200

    refactor: Remove unnecessary background color from playerVideo section in SyncRadio App.vue

--- components/SyncRadio/App.vue
+++ components/SyncRadio/App.vue
@@ -594,7 +594,7 @@ onMounted(async () => {
       </form>
     </section>
 
-    <section id="playerVideo" class="relative flex flex-col items-center justify-center w-full h-full bg-green-500 rounded lg:row-start-2 lg:col-start-2 lg:col-end-3 aspect-video lg:aspect-auto">
+    <section id="playerVideo" class="relative flex flex-col items-center justify-center w-full h-full rounded lg:row-start-2 lg:col-start-2 lg:col-end-3 aspect-video lg:aspect-auto">
       <p v-if="errorMessage" class="p-5 text-lg font-semibold">
         You are probably using your YouTube account on another page or device.
         Consider stopping it to fully enjoy SyncRadio.
