0662cf8c45c83d64cdf1f105c6a8637d2e8acea6
Pierrick TAÏLY <pierrick.tly@gmail.com>
2025-02-16 00:26:54 +0100

    fix: update nuxt config for static generation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	ssr: false,
+	nitro: {
+		preset: 'netlify',
+		static: true
+	},
+	app: {
+		baseURL: '/',
+		buildAssetsDir: '/_nuxt/'
+	},
 	runtimeConfig: {
 		public: {
 			FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
@@ -32,11 +40,6 @@ export default defineNuxtConfig({
 		payloadExtraction: false,
 	},
 
-	nitro: {
-		preset: 'netlify',
-		serveStatic: true,
-	},
-
 	typescript: {
 		strict: true,
 		typeCheck: false,
@@ -56,33 +59,31 @@ export default defineNuxtConfig({
 
 	modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxt/image'],
 
-	app: {
-		head: {
-			htmlAttrs: {
-				lang: 'fr',
-			},
-			link: [
-				{
-					rel: 'icon',
-					type: 'image/x-icon',
-					href: '/favicon.ico',
-				},
-				{
-					rel: 'preconnect',
-					href: 'https://fonts.googleapis.com',
-				},
-				{
-					rel: 'preconnect',
-					href: 'https://fonts.gstatic.com',
-					crossorigin: true,
-				},
-				{
-					rel: 'stylesheet',
-					href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
-				},
-			],
-			meta: [{ name: 'theme-color', content: '#9E0102' }],
+	head: {
+		htmlAttrs: {
+			lang: 'fr',
 		},
+		link: [
+			{
+				rel: 'icon',
+				type: 'image/x-icon',
+				href: '/favicon.ico',
+			},
+			{
+				rel: 'preconnect',
+				href: 'https://fonts.googleapis.com',
+			},
+			{
+				rel: 'preconnect',
+				href: 'https://fonts.gstatic.com',
+				crossorigin: true,
+			},
+			{
+				rel: 'stylesheet',
+				href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+			},
+		],
+		meta: [{ name: 'theme-color', content: '#9E0102' }],
 	},
 
 	vue: {
